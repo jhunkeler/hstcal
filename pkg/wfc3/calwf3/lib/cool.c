f753661804c4c71a1807039eb1ec336cf1a6c7d5
Joseph Hunkeler <jhunkeler@gmail.com>
2016-03-14 23:26:49 -0400

    Add cool new feature

---
+++ pkg/wfc3/calwf3/lib/cool.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+void cool();
+
+void cool()
+{
+	/* Hard work, math, and science */
+	int neat;
+	int stuff;
+	int occurred;
+ 
+	neat = 1;
+	stuff = 1;
+	occurred = neat + stuff;
+ 
+	print("%d + %d = %d\n", neat, stuff, occurred);
+}
